ceca7bc36fda0d3e00668ddcafa542a0e6e209e4
François Magimel <francois.magimel@etu.enseeiht.fr>
2014-03-11 00:18:50 +0100

    Created model for the Polls app' with tests.

--- myproject/myapp/tests.py
+++ myproject/myapp/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from myapp.models import Poll
 
-# Create your tests here.
+import datetime
+
+
+class TestPollModel(TestCase):
+    def test_poll(self):
+        first_poll = Poll()
+        first_question = "What is your age?"
+        first_poll.question = first_question
+        first_poll.save()
+
+        saved_polls = Poll.objects.all()
+        first_saved_poll = saved_polls[0]
+
+        self.assertEquals(saved_polls.count(), 1)
+        self.assertEquals(first_saved_poll.question, first_question)
+
+
