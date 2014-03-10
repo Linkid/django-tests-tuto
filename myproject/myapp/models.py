2536c49441e5ac5c0ab753b421cfb73d248dea0b
François Magimel <francois.magimel@etu.enseeiht.fr>
2014-03-11 00:37:49 +0100

    Added the Poll.pub_date attribute

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -3,3 +3,4 @@ from django.db import models
 
 class Poll(models.Model):
     question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
--- myproject/myapp/tests.py
+++ myproject/myapp/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.utils.timezone import utc
 from myapp.models import Poll
 
 import datetime
@@ -8,7 +9,9 @@ class TestPollModel(TestCase):
     def test_poll(self):
         first_poll = Poll()
         first_question = "What is your age?"
+        first_date = datetime.datetime(2014, 3, 11, 0, 1).replace(tzinfo=utc)
         first_poll.question = first_question
+        first_poll.pub_date = first_date
         first_poll.save()
 
         saved_polls = Poll.objects.all()
@@ -16,5 +19,4 @@ class TestPollModel(TestCase):
 
         self.assertEquals(saved_polls.count(), 1)
         self.assertEquals(first_saved_poll.question, first_question)
-
-
+        self.assertEquals(first_saved_poll.pub_date, first_date)
